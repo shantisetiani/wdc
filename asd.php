8255826e40fe0dceac620a24c4ee8a815f98ebed
shantisetiani <shantisetiani01@gmail.com>
2017-10-13 00:28:17 +0700

    :v

---
+++ asd.php
@@ -0,0 +1,34 @@
+<?php include( 'header.php' ); ?>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-m-1">a</div>
+			<div class="col-m-1">a</div>
+			<div class="col-m-1">a</div>
+			<div class="col-m-1">a</div>
+			<div class="col-m-1">a</div>
+			<div class="col-m-1">a</div>
+			<div class="col-m-1">a</div>
+			<div class="col-m-1">a</div>
+			<div class="col-m-1">a</div>
+			<div class="col-m-1">a</div>
+			<div class="col-m-1">a</div>
+			<div class="col-m-1">a</div>
+		</div>
+		<div class="row">
+			<div class="col-m-1">a</div>
+			<div class="col-m-1">a</div>
+			<div class="col-m-1">a</div>
+			<div class="col-m-1">a</div>
+			<div class="col-m-1">a</div>
+			<div class="col-m-1">a</div>
+			<div class="col-m-1">a</div>
+			<div class="col-m-1">a</div>
+			<div class="col-m-1">a</div>
+			<div class="col-m-1">a</div>
+			<div class="col-m-1">a</div>
+			<div class="col-m-1">a</div>
+		</div>
+	</div>
+
+<?php include( 'footer.php' ); ?>
