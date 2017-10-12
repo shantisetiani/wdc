08b5a646a72fd6fa93f507f4647488a7a829d81f
shantisetiani <shantisetiani01@gmail.com>
2017-10-13 00:23:58 +0700

    :p

---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+    <nav class="nav">
+        <ul class="left"><img src=""></ul>
+        <ul class="identity right">
+            <span><img src=""></span>
+            <span>Hi,</span>
+        </ul>
+    </nav>
+    <nav class="nav-left">
+        <div class="board nav-link"><a href="#">Home</a></div>
+        <div class="nav-link"><a href="#">Statistic</a></div>
+        <div class="nav-link"><a href="#">Live Support</a></div>
+        <div class="nav-link"><a href="#">Forum</a></div>
+        <div class="nav-link"><a href="#">News & Events</a></div>
+        <div class="nav-link"><a href="#">Financial</a></div>
+    </nav>
+</body>
+</html>
